efcdd25f4cb2350527c7919302bf9525bf5fe90b
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2026-03-01 21:11:35 +0100

    hw-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,37 @@
-console.log('Form');
+const formData = {
+  email: '',
+  message: '',
+};
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+const savedData = localStorage.getItem('feedback-form-state');
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+}
